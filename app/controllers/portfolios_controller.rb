a1bd2ea5c75215151debfaf0c62108aa78ae9ab4
ssaifee <shabbirsaifee91@gmail.com>
2019-02-12 13:07:33 -0500

    Integrate edit feature for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,7 @@
 class PortfoliosController < ApplicationController
 
+  before_action :set_portfolio_item, only: [:edit, :update]
+
   def index
     @portfolio_items = Portfolio.all
   end
@@ -19,9 +21,26 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @portfolio_item.update portfolio_params
+        format.html { redirect_to portfolios_path, notice: 'Your Portfolio item is updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
   private
 
   def portfolio_params
     params.require(:portfolio).permit(:title, :subtitle, :body)
+  end
+
+  def set_portfolio_item
+    @portfolio_item = Portfolio.find params[:id]
   end
 end
