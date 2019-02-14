8c7b2f49c1a9d4556bd6b40db697c77797295d52
ssaifee <shabbirsaifee91@gmail.com>
2019-02-13 23:25:52 -0500

    Implement concern for managing image data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,6 +1,7 @@
 class Portfolio < ApplicationRecord
   validates :title, :subtitle, :body, :main_image, :thumb_image, presence: true
 
+  include Placeholder
   scope :angular, -> { where(subtitle: 'Angular') }
 
   after_initialize :set_defaults
@@ -8,7 +9,7 @@ class Portfolio < ApplicationRecord
   private
 
   def set_defaults
-    self.main_image ||= 'https://via.placeholder.com/600x400'
-    self.thumb_image ||= 'https://via.placeholder.com/350x200'
+    self.main_image ||= image_generator(width: 600, height: 400)
+    self.thumb_image ||= image_generator(width: 350, height: 200)
   end
 end
