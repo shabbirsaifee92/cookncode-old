b1b8d5412a384ae5904b29532ec3793837f567ac
ssaifee <shabbirsaifee91@gmail.com>
2019-02-22 19:09:22 -0500

    Add navigation and default header content

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,12 +4,14 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
+    @page_title = "My portfolio blog"
     @blogs = Blog.all
   end
 
   # GET /blogs/1
   # GET /blogs/1.json
   def show
+    @page_title = @blog.title
   end
 
   # GET /blogs/new
