b304ef68319b39de49e1bb4b2ab1c9496da5c926
ssaifee <shabbirsaifee91@gmail.com>
2019-02-27 20:27:39 -0500

    Implement view helper for auth lins

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def sample_helper
+    "<p> Hello Helper<p>".html_safe
+    "<div> <h2>This is another helper message </h2> </div>".html_safe
+  end
+
+  def login_helper
+    if current_user.is_a?(User)
+      link_to 'Logout', destroy_user_session_path, method: :delete
+    else
+      (link_to 'Sign Up', new_user_registration_path) + ' ' +
+      (link_to 'Login', new_user_session_path)
+    end
+  end
 end
