0f4d7f541b9bb7ec85b0065977550cea735339c0
ssaifee <shabbirsaifee91@gmail.com>
2019-03-04 20:33:06 -0500

    Implement content tag helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,16 @@
 module ApplicationHelper
-  def sample_helper
-    "<p> Hello Helper<p>".html_safe
-    "<div> <h2>This is another helper message </h2> </div>".html_safe
-  end
-
   def login_helper
     if current_user.is_a?(User)
       link_to 'Logout', destroy_user_session_path, method: :delete
     else
       (link_to 'Sign Up', new_user_registration_path) + ' ' +
       (link_to 'Login', new_user_session_path)
+    end
+  end
+
+  def source_helper(layout)
+    if session[:source].present?
+      content_tag :p, "Thank for visiting me from #{session[:source]}", class: "#{layout}-source-greeting"
     end
   end
 end
