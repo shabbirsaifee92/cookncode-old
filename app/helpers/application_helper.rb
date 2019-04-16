a92587d18233411b9a6fde6761bb2be59647a218
= <shabbirsaifee91@gmail.com>
2019-04-15 20:33:30 -0400

    Implement header footer style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,11 @@
 module ApplicationHelper
-  def login_helper
+  def login_helper(css_class)
     if current_user.is_a?(GuestUser)
-      (link_to 'Sign Up', new_user_registration_path) + ' ' +
-      (link_to 'Login', new_user_session_path)
+      (link_to 'Sign Up', new_user_registration_path, class: css_class) + 
+      ' '.html_safe +
+      (link_to 'Login', new_user_session_path, class: css_class)
     else
-      link_to 'Logout', destroy_user_session_path, method: :delete
+      link_to 'Logout', destroy_user_session_path, method: :delete, class: css_class
     end
   end
 
