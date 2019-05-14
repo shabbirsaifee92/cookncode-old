b869a83bd60f7e6afc8dcf1999b5448273f6bb5a
= <shabbirsaifee91@gmail.com>
2019-05-13 22:07:19 -0400

    Add activestorage for image uploads

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -8,17 +8,13 @@ class Portfolio < ApplicationRecord
 
   validates :title, :subtitle, :body, :main_image, :thumb_image, presence: true
 
+  has_one_attached :thumb_image
+  has_one_attached :main_image
+
   scope :angular, -> { where(subtitle: 'Angular') }
   scope :by_position, -> { order('position') }
 
-  after_initialize :set_defaults
-
   private
-
-  def set_defaults
-    self.main_image ||= image_generator(width: 600, height: 400)
-    self.thumb_image ||= image_generator(width: 350, height: 200)
-  end
 
   def self.update_positions(position_hash)
     position_hash.each do |_key, value|
